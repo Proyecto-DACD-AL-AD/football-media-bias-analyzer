4e7c55fb5beea9ae9e8f7607a155d2dff51203ee
aythamilorenzo <aythamilorenzo06@gmail.com>
2026-03-19 22:51:39 +0000

    Delete method printJson in FootballApiClient class and create 2 methods for filtering and printing the matches. Also change the name of the score attribute in Score class

--- football-api-client/src/main/java/FootballApiClient.java
+++ football-api-client/src/main/java/FootballApiClient.java
@@ -1,14 +1,10 @@
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import java.util.List;
 
 public class FootballApiClient {
 
@@ -22,27 +18,51 @@ public class FootballApiClient {
         HttpRequest request = footballApiClient.getHttpRequest();
 
         try {
+
             HttpResponse<String> response = footballApiClient.getStringHttpResponse(client, request);
-            footballApiClient.printJson(response);
+            if (response.statusCode() != 200) {
+                System.out.println("Error al contactar con la API, código: " + response.statusCode());
+                return;
+            }
+
+            footballApiClient.filterMatches(response);
 
         } catch (Exception e) {
 
             System.out.println("Error de conexión: " + e.getMessage());
-
         }
-
-
-
-
     }
 
-    private void printJson(HttpResponse<String> response) {
-        String jsonCrudo = response.body();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        JsonElement jsonElement = JsonParser.parseString(jsonCrudo);
-        String json = gson.toJson(jsonElement);
+    private void filterMatches(HttpResponse<String> response) {
 
-        System.out.println(json);
+        String rawJson = response.body();
+        Gson gson = new Gson();
+        MatchListResponse matchList = gson.fromJson(rawJson, MatchListResponse.class);
+        List<String> mediaTeams = List.of("Real Madrid CF", "FC Barcelona",
+                "Real Betis Balompié", "Sevilla FC", "Athletic Club", "Real Sociedad de Fútbol");
+
+        List<MatchResponse> filteredMatches = matchList.getMatches().stream()
+                .filter(match -> "FINISHED".equals(match.getStatus()))
+                .filter(match ->
+                        mediaTeams.contains(match.getHomeTeam().getName()) ||
+                                mediaTeams.contains(match.getAwayTeam().getName())
+                ).toList();
+
+        printFilteredMatches(filteredMatches);
+    }
+
+    private static void printFilteredMatches(List<MatchResponse> filteredMatches) {
+
+        for (MatchResponse match : filteredMatches) {
+            String homeTeam = match.getHomeTeam().getName();
+            String awayTeam = match.getAwayTeam().getName();
+
+            int homeGoals = match.getScore().getFullTime().getHomeGoals();
+            int awayGoals = match.getScore().getFullTime().getAwayGoals();
+            String result = homeGoals + " - " + awayGoals;
+
+            System.out.println(homeTeam + " [" + result + "] " + awayTeam + " | Status: " + match.getStatus());
+        }
     }
 
     private HttpResponse<String> getStringHttpResponse(HttpClient client, HttpRequest request) throws IOException,
@@ -59,3 +79,4 @@ public class FootballApiClient {
                 .build();
     }
 }
+
