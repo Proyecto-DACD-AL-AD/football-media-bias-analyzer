a4e71e31daf67cea013ce49eeecd0e3d8e301db5
aythamilorenzo <aythamilorenzo06@gmail.com>
2026-03-22 20:04:00 +0000

    Create 3 classes to extract the position and team from the standings

---
+++ football-api-client/src/main/java/StandingsResponse.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class StandingsResponse {
+
+    private List<Standing> standings;
+
+    public List<Standing> getStandings() {
+        return standings;
+    }
+}
