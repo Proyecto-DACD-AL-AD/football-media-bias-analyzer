6fa4f25c526e32aa0d8a725d316aaf2713a5fa95
aythamilorenzo <aythamilorenzo06@gmail.com>
2026-03-22 19:56:36 +0000

    Change FootballApiClient class to extract the standings info from the API and refactor the code

--- football-api-client/src/main/java/FootballApiClient.java
+++ football-api-client/src/main/java/FootballApiClient.java
@@ -8,43 +8,41 @@ import java.net.http.HttpResponse;
 public class FootballApiClient {
 
     private static final String API_TOKEN = "b16a3ee0850b41b7b3bc5ee95b7061dc";
-    private static final String API_URL = "https://api.football-data.org/v4/competitions/2014/matches?season=2025";
+    private static final String BASE_URL = "https://api.football-data.org/v4/competitions/2014/";
     private final HttpClient client = HttpClient.newHttpClient();
 
 
     public String getAllMatches() throws IOException, InterruptedException {
-        HttpRequest request = getHttpRequest();
-        HttpResponse<String> response = getHttpResponse(request);
+        String matchesUrl = BASE_URL + "matches?season=2025";
+        return makeApiCall(matchesUrl);
+    }
 
+    public String getStandingsByMatchday (int matchday) throws IOException, InterruptedException {
+        String standingsUrl = BASE_URL + "standings?season=2025&matchday=" + matchday;
+        return makeApiCall(standingsUrl);
+    }
+
+    private String makeApiCall(String url) throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("X-Auth-Token", API_TOKEN)
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Llamando a: " + url);
+        System.out.println("Código de estado: " + response.statusCode());
 
         checkStatusCodeError(response);
 
         return response.body();
     }
 
-
     private static void checkStatusCodeError(HttpResponse<String> response) {
         if (response.statusCode() != 200) {
             throw new RuntimeException("Error al contactar con la API, código: " + response.statusCode());
         }
     }
-
-
-    private HttpResponse<String> getHttpResponse(HttpRequest request)
-            throws IOException, InterruptedException {
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("Código de estado: " + response.statusCode());
-        return response;
-    }
-
-
-    private HttpRequest getHttpRequest() {
-        return HttpRequest.newBuilder()
-                .uri(URI.create(API_URL))
-                .header("X-Auth-Token", API_TOKEN)
-                .build();
-    }
-
 }
 
